ab35e7b7f4386ea601293e011b86cf9efdbd1d6d
Alexander Bier <a.bier@global-communication.de>
2021-06-01 10:05:56 +0200

    added debug mode with program argument "debug"

--- src/personenkartei/Main.java
+++ src/personenkartei/Main.java
@@ -4,17 +4,32 @@ import personenkartei.views.StartUp;
 
 import java.awt.*;
 
-public class Main  {
+public class Main {
+
+    private static boolean debugMode = false;
 
     public static Dimension getScreenDimension() {
         return new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
     }
 
     public static void debug(Object object) {
-        System.out.println(object);
+        if (Main.debugMode) {
+            System.out.println(object);
+        }
     }
 
     public static void main(String[] args) {
+        for (String arg : args) {
+            if (arg.equals("debug")) {
+                Main.debugMode = true;
+                break;
+            }
+        }
+        if (!Main.debugMode) {
+            System.out.println("If you want to put the application in debug mode, add the program argument: debug");
+        }
+        Main.debug("Debug mode is enabled");
+
         new StartUp(getScreenDimension()).show();
     }
 }
